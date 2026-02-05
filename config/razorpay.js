79f462a722d833946a7461c1516b37ce79edf619
RahulRaoMR <royalrahulraomr9103@gmail.com>
2026-02-05 16:57:33 +0530

    Fix Razorpay env variables

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,8 +1,11 @@
-const Razorpay = require("razorpay");
+import Razorpay from "razorpay";
 
 const razorpay = new Razorpay({
   key_id: rzp_test_SCPdofdl0uo5pU,
   key_secret:XM6IeqFOz5FPtxE5hvLkbQj6,
 });
 
-module.exports = razorpay;
+export default razorpay;
+
+
+
